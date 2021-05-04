aaffb6c3e4fc9d9933df604953533eaad65f0a26
Jeroen Ooms <jeroenooms@gmail.com>
2021-05-04 12:07:34 +0200

    Update to latest rtools40 build

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
   Url            = 'https://cloud.r-project.org/bin/windows/Rtools/rtools40-i686.exe'
-  Url64bit       = 'https://cloud.r-project.org/bin/windows/Rtools/rtools40-x86_64.exe'
+  Url64bit       = 'https://cloud.r-project.org/bin/windows/Rtools/rtools40v2-x86_64.exe'
   Checksum       = '00ef9bb70c83c2397ec8460a7522039a7ebe5b53d22dcb7a5aba924ee6d07eb5bbc513b53f2d25136145173a173f0bfca435ee003fba6d3d1c8426bd5d8660a9'
-  Checksum64       = '2783f2ebbdde6f797f9472e2b8000e8bde01c9b5094bb8ef402fe05b5e7cc606e71ad2fad89194c2a28d809f0bb14590b7919eaf83854ecdf14832de80eda530'
+  Checksum64       = '2bd13859a223dbf16585123f795a0b3f2be6f053a10ab8a01920b32f9989c710e8f3dd230eda8f49ed73fbe11cb9f0c50b268c0ff30089127c1e89ba0058fad6'
   ChecksumType   = 'sha512'
   ChecksumType64 = 'sha512'
 }
