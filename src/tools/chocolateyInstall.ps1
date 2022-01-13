f13455247bbecfdca567d4ff092bb9382c9ec874
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-13 12:49:45 +0100

    Bump version, update to permanent url

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
   PackageName    = 'rtools'
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
-  Url            = 'https://cloud.r-project.org/bin/windows/Rtools/rtools40-i686.exe'
-  Url64bit       = 'https://cloud.r-project.org/bin/windows/Rtools/rtools40v2-x86_64.exe'
+  Url            = 'https://github.com/r-windows/rtools-installer/releases/download/2020-05-05/rtools40-i686.exe'
+  Url64bit       = 'https://github.com/r-windows/rtools-installer/releases/download/2022-01-12/rtools40-x86_64.exe'
   Checksum       = '00ef9bb70c83c2397ec8460a7522039a7ebe5b53d22dcb7a5aba924ee6d07eb5bbc513b53f2d25136145173a173f0bfca435ee003fba6d3d1c8426bd5d8660a9'
-  Checksum64       = '2bd13859a223dbf16585123f795a0b3f2be6f053a10ab8a01920b32f9989c710e8f3dd230eda8f49ed73fbe11cb9f0c50b268c0ff30089127c1e89ba0058fad6'
+  Checksum64     = 'db15d246970308ce624e679d5d0b50e7a837871e3762b7b7c4df06c40e8f3096aed51c0c229f906bd21a4736b016eb9ae716127f86b1d7da4df412e2a3280732'
   ChecksumType   = 'sha512'
   ChecksumType64 = 'sha512'
 }
