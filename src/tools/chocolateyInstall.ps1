6d417eec79927d8558058f85014398b80c1c13e5
Jeroen Ooms <jeroenooms@gmail.com>
2025-02-25 11:04:35 +0100

    Bump to 4.4.6459

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
   Url            = 'https://github.com/r-windows/rtools-installer/releases/download/2020-05-05/rtools40-i686.exe'
-  Url64bit       = 'https://github.com/r-windows/rtools-chocolatey/releases/download/6414/rtools44-6414-6401.exe'
+  Url64bit       = 'https://github.com/r-windows/rtools-chocolatey/releases/download/6459/rtools44-6459-6401.exe'
   Checksum       = '00ef9bb70c83c2397ec8460a7522039a7ebe5b53d22dcb7a5aba924ee6d07eb5bbc513b53f2d25136145173a173f0bfca435ee003fba6d3d1c8426bd5d8660a9'
-  Checksum64     = 'c0b25d406eb5b780d665896f9a657daf980fabed260146ff36cc40e46c6ffed6bb79d9e7aa5fc4115e2c1ca1683efe2794aaeade1037bfd3f119fc5c9450fd13'
+  Checksum64     = 'ba99aa70defef3c34c9d3d02aeeb19f82207bd4ffa0c3c5411ae9202d4da73f14b62cdd5076f619e9b6d21a8a38bc616e014d4380f4ce89c99d95e341f8174d9'
   ChecksumType   = 'sha512'
   ChecksumType64 = 'sha512'
 }
