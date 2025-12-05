64c4996c3796f784a7b401e1e33a99265ecf9554
Jeroen Ooms <jeroenooms@gmail.com>
2025-12-05 11:51:56 +0100

    Bump version for R-4.5.2

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -3,11 +3,10 @@
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
   Url            = 'https://github.com/r-windows/rtools-installer/releases/download/2020-05-05/rtools40-i686.exe'
-  Url64bit       = 'https://github.com/r-windows/rtools-chocolatey/releases/download/6608/rtools45-6608-6492.exe'
+  Url64bit       = 'https://github.com/r-windows/rtools-chocolatey/releases/download/6691/rtools45-6691-6492.exe'
   Checksum       = '00ef9bb70c83c2397ec8460a7522039a7ebe5b53d22dcb7a5aba924ee6d07eb5bbc513b53f2d25136145173a173f0bfca435ee003fba6d3d1c8426bd5d8660a9'
-  Checksum64     = '5176a077bebabd549379cab9629919027b8a317b0695db073b757972209cc1f998636be42bee25230385b4ff02d93d7dd4ad25ee459deb7c7183bd115b569765'
+  Checksum64     = '5720b6bc5a63f568b35c9c82a48838f0542f99368d83a9021d44e85a8c7c34ba16a42774041daaeaca52252e5f00c455c5f3d7db242ca1c7cf4a99a530076a2d'
   ChecksumType   = 'sha512'
   ChecksumType64 = 'sha512'
 }
- 
 Install-ChocolateyPackage @packageArgs
